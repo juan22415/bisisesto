c1ae4aaf713692ca58ca51aa577054424f65d8f6
juan camilo rios <juan22415@hotmail.com>
2017-08-17 15:52:41 -0500

    Pruebas unitarias

---
+++ Assets/Editor/NewEditModeTest.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NewEditModeTest {
+
+
+
+	[Test]
+	public void Es1948bisiesto() {
+
+		Bisiesto bisiesto = new Bisiesto();
+
+		int year = bisiesto.esbisisesto (1948);
+
+		int yearoperation = year % 4;
+
+
+		Assert.AreEqual (0, yearoperation);
+
+	}
+
+	[Test]
+	public void es1800bisiesto() {
+
+		Bisiesto bisiesto = new Bisiesto();
+
+		int year = bisiesto.esbisisesto (1800);
+
+		int yearoperation = year % 100;
+
+
+		Assert.AreEqual (0, yearoperation);
+
+	}
+
+	[Test]
+	public void Es2000bisiesto() {
+
+		Bisiesto bisiesto = new Bisiesto();
+
+		int year = bisiesto.esbisisesto (2000);
+
+		int yearoperation = year % 400;
+
+
+		Assert.AreEqual (0, yearoperation);
+
+	}
+
+	}
+	
